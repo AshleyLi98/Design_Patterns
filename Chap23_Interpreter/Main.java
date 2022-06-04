a6254ceaa600a6d9e93abecb8af9ac656c1adc40
lishuting <im_shutingli@163.com>
2022-06-04 19:09:07 +0800

    Interpreter模式

---
+++ Chap23_Interpreter/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("program.txt"));
+			String text;
+			while((text = reader.readLine()) != null) {
+				System.out.println("text = \"" + text + "\"");
+				Node node = new ProgramNode();
+				node.parse(new Context(text));
+				System.out.println("node = " + node);
+			}
+			reader.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
